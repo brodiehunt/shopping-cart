ba96b3dc6a6e6cb874e1a5628299912b68ee3766
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-12-06 07:19:46 +1100

    Home component completed - jsx, styles (mobile - desktop)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,48 @@
+import {Link} from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>This is the home</div>
+    <div className="home-container">
+      <section className="hero-container">
+        <img src="landing-img.jpg" alt="pair of white shoes on a gray background"/>
+        <div className="hero-content">
+          <h1>Find <br/>your <br/>feet.</h1>
+          <button className="hero-btn">
+            <Link to='/products'>Shop Now</Link>
+          </button>
+        </div>
+      </section>
+      <section className="category-container">
+        <h2>Shop by category</h2>
+        <div className="category-grid">
+          <Link to="/products" className="grid-item-container">
+            <div className="grid-item">
+              <img src='unisex.jpg' alt="White leather unisex sneakers"/>
+              <div className="grid-item-text">
+                Shop All
+              </div>
+            </div>
+          </Link>
+          <Link to="/products/category/mens" className="grid-item-container">
+            <div className="grid-item">
+              <img src='mens.jpg' alt="White nike hightops on concrete"/>
+              <div className="grid-item-text">
+                Shop Mens
+              </div>
+            </div>
+          </Link>
+          <Link to="/products/category/womens" className="grid-item-container">
+            <div className="grid-item">
+              <img src='womens.jpg' alt="Colorful sneakers on a pedastool"/>
+              <div className="grid-item-text">
+                Shop Womens
+              </div>
+            </div>
+          </Link>
+        </div>
+      </section>
+
+    </div>
   )
 }
 
